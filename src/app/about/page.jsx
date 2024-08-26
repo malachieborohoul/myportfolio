f52e45d7d323dd457bee22a1379d540eb0b3096f
malachieborohoul <malachieborohoul@gmail.com>
2024-08-26 08:45:55 +0100

    New version porfolio

---
+++ src/app/about/page.jsx
@@ -0,0 +1,112 @@
+import Image from 'next/image'
+import Link from 'next/link'
+import clsx from 'clsx'
+
+import { Container } from '@/components/Container'
+import {
+  GitHubIcon,
+  MediumIcon,
+  LinkedInIcon,
+  XIcon,
+} from '@/components/SocialIcons'
+import portraitImage from '@/images/avatar.jpeg'
+
+function SocialLink({ className, href, children, icon: Icon }) {
+  return (
+    <li className={clsx(className, 'flex')}>
+      <Link
+        href={href}
+        className="group flex text-sm font-medium text-zinc-800 transition hover:text-teal-500 dark:text-zinc-200 dark:hover:text-teal-500"
+      >
+        <Icon className="h-6 w-6 flex-none fill-zinc-500 transition group-hover:fill-teal-500" />
+        <span className="ml-4">{children}</span>
+      </Link>
+    </li>
+  )
+}
+
+function MailIcon(props) {
+  return (
+    <svg viewBox="0 0 24 24" aria-hidden="true" {...props}>
+      <path
+        fillRule="evenodd"
+        d="M6 5a3 3 0 0 0-3 3v8a3 3 0 0 0 3 3h12a3 3 0 0 0 3-3V8a3 3 0 0 0-3-3H6Zm.245 2.187a.75.75 0 0 0-.99 1.126l6.25 5.5a.75.75 0 0 0 .99 0l6.25-5.5a.75.75 0 0 0-.99-1.126L12 12.251 6.245 7.187Z"
+      />
+    </svg>
+  )
+}
+
+export const metadata = {
+  title: 'About',
+  description:
+    'I’m Spencer Sharp. I live in New York City, where I design the future.',
+}
+
+export default function About() {
+  return (
+    <Container className="mt-16 sm:mt-32">
+      <div className="grid grid-cols-1 gap-y-16 lg:grid-cols-2 lg:grid-rows-[auto_1fr] lg:gap-y-12">
+        <div className="lg:pl-20">
+          <div className="max-w-xs px-2.5 lg:max-w-none">
+            <Image
+              src={portraitImage}
+              alt=""
+              sizes="(min-width: 1024px) 32rem, 20rem"
+              className="aspect-square  rounded-2xl bg-zinc-100 object-cover dark:bg-zinc-800"
+            />
+          </div>
+        </div>
+        <div className="lg:order-first lg:row-span-2">
+          <h1 className="text-4xl font-bold tracking-tight text-zinc-800 sm:text-5xl dark:text-zinc-100">
+          I&apos;m BOROHOUL Soguelni Malachie. A software engineer.
+          </h1>
+          <div className="mt-6 space-y-7 text-base text-zinc-600 dark:text-zinc-400">
+            <p>
+            Skilled Software Engineer with <b>over 2 years of experience</b> in
+                mobile and web application development. Expertise in <b>Flutter,
+                Node.js, .NET, and React.js</b>, with a strong focus on <b>Clean
+                Architecture and TDD</b>. Successfully reduced production time by
+                50% through CI/CD pipeline implementation on <b>Azure</b>. Developed
+                key projects including a salon booking app, a Mission Tracking
+                Management API, and a mobile E-Learning platform. Holds a
+                Bachelor&apos;s degree in Software Engineering from the University
+                Institute of Technology of Ngaoundéré.
+            </p>
+            <p>
+                <b>Fluent</b> in English and French.
+              </p>
+              <p>
+                <b>Areas of Expertise:</b> Software Engineering, Mobile Development, Full Stack Development, DevOps
+              </p>
+              <p>Feel free to reach out to me.</p>
+              <p>Have a great day!</p>
+          </div>
+        </div>
+        <div className="lg:pl-20">
+          <ul role="list">
+          <SocialLink href="https://medium.com/@malachieborohoul" icon={MediumIcon} >
+              Follow on Medium
+            </SocialLink>
+            <SocialLink href="https://x.com/malachiborohoul" icon={XIcon} className="mt-4">
+              Follow on X
+            </SocialLink>
+          
+            <SocialLink href="https://github.com/malachieborohoul" icon={GitHubIcon} className="mt-4">
+              Follow on GitHub
+            </SocialLink>
+            <SocialLink href="https://www.linkedin.com/in/borohoul-soguelni-malachie-49b3251a4" icon={LinkedInIcon} className="mt-4">
+              Follow on LinkedIn
+            </SocialLink>
+            <SocialLink
+              href="mailto:malachiborohoul@gmail.com"
+              icon={MailIcon}
+              className="mt-8 border-t border-zinc-100 pt-8 dark:border-zinc-700/40"
+            >
+              malachiborohoul@gmail.com
+            </SocialLink>
+          </ul>
+        </div>
+      </div>
+    </Container>
+  )
+}
---
+++ src/app/layout.jsx
@@ -0,0 +1,33 @@
+import { Providers } from '@/app/providers'
+import { Layout } from '@/components/Layout'
+
+import '@/styles/tailwind.css'
+
+export const metadata = {
+  title: {
+    template: '%s - BOROHOUL Soguelni Malachie',
+    default:
+      " I'm BOROHOUL Soguelni Malachie. A software engineer.",
+  },
+  description:
+    ' My name is BOROHOUL Soguelni Malachie, a mobile developer, full stack developer, and software engineer. I am passionate about building software and exploring the world of AI.',
+  alternates: {
+    types: {
+      'application/rss+xml': `${process.env.NEXT_PUBLIC_SITE_URL}/feed.xml`,
+    },
+  },
+}
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en" className="h-full antialiased" suppressHydrationWarning>
+      <body className="flex h-full bg-zinc-50 dark:bg-black">
+        <Providers>
+          <div className="flex w-full">
+            <Layout>{children}</Layout>
+          </div>
+        </Providers>
+      </body>
+    </html>
+  )
+}
