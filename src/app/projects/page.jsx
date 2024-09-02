fa6571efd03688638f0394f7fdf5786f515d6a94
malachieborohoul <malachieborohoul@gmail.com>
2024-09-02 22:34:16 +0100

    Update show label icon according to lenght

--- src/app/projects/page.jsx
+++ src/app/projects/page.jsx
@@ -115,7 +115,7 @@ export default function Projects() {
                 <span className="ml-2">{project.link.label}</span>
               </a>
               <a href={project.store.href} className="relative z-10 mt-6 flex text-sm font-medium text-zinc-400 transition group-hover:text-teal-500 dark:text-zinc-200">
-                <LinkIcon className="h-6 w-6 flex-none" />
+               {project.store.label.length > 0 ? <LinkIcon className="h-6 w-6 flex-none" /> : ''} 
                 <span className="ml-2">{project.store.label}</span>
               </a>
 
